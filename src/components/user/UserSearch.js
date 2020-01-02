ec977adaa869fa7757e35ee50e8ec9a849019a34
Kristen Norris <krnorris65@gmail.com>
2020-01-01 22:09:35 -0600

    user can enter something to search and when a button is pressed it gets all the users

--- src/components/user/UserSearch.js
+++ src/components/user/UserSearch.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import ApiManager from '../../modules/ApiManager'
 
 const UserSearch = props => {
 
-    console.log("User Search Page")
+    const searchedName = useRef()
+
+
+    const findUser = () => {
+        const searched = searchedName.current.value.toLowerCase()
+        ApiManager.getAll("users")
+        .then(allUsers => {
+            console.log(allUsers)
+            console.log(searched)
+        })
+    }
 
     return (
         <>
         <h2>Find Users</h2>
+
+        <input type="text" ref={searchedName} placeholder="Search for a user"/>
+        <button onClick={findUser}>Search</button>
         </>
     )
 
