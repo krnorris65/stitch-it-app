62d72e20d62a34e068f869488c3d97881086b95b
Kristen Norris <krnorris65@gmail.com>
2020-01-06 11:49:02 -0600

    added comments for what each method should do

--- src/components/user/UserHome.js
+++ src/components/user/UserHome.js
@@ -7,15 +7,23 @@ import UserList from './UserList'
 const UserHome = props => {
 
     const followUser = () => {
+        // if a user wants to follow a user, create a data point in the follows resource. 
+        // if their profile is public then pending = false
+        // if their private is public then pending = true
 
     }
 
     const deleteFollow = () => {
+        //delete a data point in the follows resource
+        //if user is currently following a user but wants to unfollow them
+        //if a private user declines another user's follow request
+        //if a user has a follow request for a private user and wants to delete the follow request before it's been approved or declined
 
     }
 
-
-
+    const approveFollow = () => {
+        //if a user with a private profile approves a pending follow request, change pending to false
+    }
 
 
     return (
