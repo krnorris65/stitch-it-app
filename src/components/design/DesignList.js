704c969fc41f91c3ecffc3a1cfdc2e4d012323e1
Kristen Norris <krnorris65@gmail.com>
2020-01-20 11:47:59 -0600

    refactored naming conventions on Design List

--- src/components/design/DesignList.js
+++ src/components/design/DesignList.js
@@ -3,19 +3,20 @@ import DesignCard from './DesignCard'
 import { DesignContext } from "../providers/DesignProvider"
 
 const DesignList = props => {
-    const [otherUserInfo, setOtherInfo] = useState(props.location.state)
-    const [otherDesigns, setOtherDesigns] = useState([])
+    const followedUser = props.match.path.includes('following')
+    const [followedUserInfo, setFollowedInfo] = useState()
+    const [followedDesigns, setFollowedDesigns] = useState([])
 
 
     let { designs, getOtherUserDesigns } = useContext(DesignContext)
 
     const checkIfOtherUser = () => {
-        if (otherUserInfo) {
+        if (followedUser) {
             //change user info when toggling between followed users profiles
-            setOtherInfo(props.location.state)
+            setFollowedInfo(props.location.state)
             //get the other users designs
             getOtherUserDesigns(props.match.params.userId)
-                .then(setOtherDesigns)
+                .then(setFollowedDesigns)
         }
     }
 
@@ -24,7 +25,7 @@ const DesignList = props => {
     return (
         <>
             {
-                (!otherUserInfo) ?
+                (!followedUser) ?
                     <>
                         <section>
                             <button onClick={() => props.history.push("/design/new")}>Add New Design</button>
@@ -33,13 +34,14 @@ const DesignList = props => {
                             {designs.map(design => <DesignCard key={design.id} design={design} {...props} />)}
                         </div>
                     </>
-                    :
+                    : (followedUserInfo) &&
                     <>
-                        <h2>{otherUserInfo.firstName}'s Designs</h2>
+                        <h2>{followedUserInfo.firstName}'s Designs</h2>
                         <div className="container-cards">
-                            {otherDesigns.map(design => <DesignCard key={design.id} design={design} {...props} />)}
+                            {followedDesigns.map(design => <DesignCard key={design.id} design={design} {...props} />)}
                         </div>
                     </>
+
             }
 
         </>
