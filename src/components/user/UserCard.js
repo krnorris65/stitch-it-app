5237a3e34a55f9da146bc775235f62493e88a7a1
Kristen Norris <krnorris65@gmail.com>
2020-02-04 16:06:35 -0600

    added icons to user page

--- src/components/user/UserCard.js
+++ src/components/user/UserCard.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect, useContext } from 'react'
 import { UserContext } from '../providers/UserProvider'
 import useFollowStatus from '../../hooks/ui/useFollowStatus'
 
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import PersonIcon from '@material-ui/icons/Person';
 
 const UserCard = props => {
     const [currentUser] = localStorage.getItem("currUserId")
@@ -46,27 +49,29 @@ const UserCard = props => {
     return (
         <div className="card">
             <div className="card-content">
-                <h4>{props.user.firstName} {props.user.lastName}</h4>
+                <h4 className="userName">{props.user.firstName} {props.user.lastName}</h4>
 
                 {
                     // conditional that checks if the user is the one that's logged in
 
                     (followedStatus === "current") ?
-                        <p>current user</p>
+                        <PersonIcon className="userIcon" onClick={() => props.history.push("/")} />
                         // if the current user is currently following the user
                         // if the current user isn't following the user and the user's profile is public
-                            : (followedStatus === "following") ?
-                                <>
-                                    <button onClick={() => viewDesigns(props.user, followId)}>View Profile</button>
-                                    {/* <button onClick={() => deleteFollow(followId)}>Unfollow</button> */}
-                                </>
-                                // if the current user has requested to follow the user but it hasn't been approved
-                                : (followedStatus === "pending") ?
-                                    <button onClick={() => deleteFollow(followId)}>Delete Follow Request</button>
-                                    : (props.user.publicProfile) ?
-                                        <button onClick={() => submitFollow(props.user.id, props.user.publicProfile)}>Follow</button>
-                                        // if the current user isn't following the user and the user's profile is private
-                                        : <button onClick={() => submitFollow(props.user.id, props.user.publicProfile)}>Request to Follow</button>
+                        : (followedStatus === "following") ?
+                            <>
+                                {/* <button onClick={() => viewDesigns(props.user, followId)}>View Profile</button> */}
+                                <AccountBoxIcon className="userIcon" onClick={() => viewDesigns(props.user, followId)} />
+                                {/* <button onClick={() => deleteFollow(followId)}>Unfollow</button> */}
+                            </>
+                            // if the current user has requested to follow the user but it hasn't been approved
+                            : (followedStatus === "pending") ?
+                                <button onClick={() => deleteFollow(followId)}>Delete Follow Request</button>
+                                : (props.user.publicProfile) ?
+                                    // <button onClick={() => submitFollow(props.user.id, props.user.publicProfile)}>Follow</button>
+                                    <PersonAddIcon className="userIcon" onClick={() => submitFollow(props.user.id, props.user.publicProfile)} />
+                                    // if the current user isn't following the user and the user's profile is private
+                                    : <button onClick={() => submitFollow(props.user.id, props.user.publicProfile)}>Request to Follow</button>
                 }
 
 
